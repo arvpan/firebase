26dd46f3bc10e2feeca8f3b9a14681b431235ed4
arvpan <arveen123@gmail.com>
2016-12-05 16:05:13 +0530

    Initial commit

---
+++ firebase.js
@@ -0,0 +1,8 @@
+var config = {
+    apiKey: "AIzaSyCZpIT0Hs5uWR_PJNdTBPxetGXerLVazK4",
+    authDomain: "myproject-969f8.firebaseapp.com",
+    databaseURL: "https://myproject-969f8.firebaseio.com",
+    storageBucket: "myproject-969f8.appspot.com",
+    messagingSenderId: "419614445442"
+  };
+  firebase.initializeApp(config);
